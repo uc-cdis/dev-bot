784a35d2894fe3db19d1d188c38ab5632df2eae1
Marcelo Costa <themarcelor@gmail.com>
2021-07-28 23:38:05 -0500

    override default slack domain to use what is whitelisted in squid

--- devbot/devbot.py
+++ devbot/devbot.py
@@ -11,7 +11,10 @@ signing_secret = os.environ["SIGNING_SECRET"]
 slack_event_adapter = SlackEventAdapter(signing_secret.strip(), "/slack/events", app)
 
 slack_token = os.environ["SLACK_API_TOKEN"]
-client = slack.WebClient(token=slack_token.strip())
+client = slack.WebClient(
+    token=slack_token.strip(), base_url="https://cdis.slack.com/api/"
+)
+BOT_ID = client.api_call("auth.test")["user_id"]
 
 
 @slack_event_adapter.on("message")
@@ -21,7 +24,13 @@ def message(payload):
     user_id = event.get("user")
     text = event.get("text")
 
-    client.chat_postMessage(channel=channel_id, text=text)
+    print(f"event: {event}")
+    print(f"channel_id: {channel_id}")
+    print(f"user_id: {user_id}")
+    print(f"text: {text}")
+
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id, text=text)
 
 
 if __name__ == "__main__":
